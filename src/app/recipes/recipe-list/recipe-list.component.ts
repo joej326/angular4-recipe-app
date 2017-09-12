de6784f0f3fa4f14b8731005e3829d58d6ec87d3
Joseph Joyce <joej326@gmail.com>
2017-09-12 10:32:06 -0700

    added recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('This is a test recipe', 'this is a test description,', 'https://media-cdn.tripadvisor.com/media/photo-s/07/a5/c9/d5/yummy-food.jpg')
+  ];
+
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+
+  
+
   constructor() { }
 
   ngOnInit() {
